1c33fef6490338714b42ef4647fa89e27be2b3e4
andreylpereira <andreylpereira@gmail.com>
2025-03-31 16:38:25 -0300

    [FIX] Correção navbar com o script

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Routers from "./routers/Routers.jsx";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootswatch/dist/sandstone/bootstrap.min.css";
 
 const App = () => {
   return <Routers />;
