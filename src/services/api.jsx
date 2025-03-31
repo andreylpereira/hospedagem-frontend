fa5d8b59c240e2a82399535c9c58db46c489cf41
Andrey Luiz Pereira <andreylpereira@gmail.com>
2025-03-30 21:49:40 -0300

    [ADD] url do service

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
 const api = axios.create({
-  baseURL: "http://localhost:8888/api",
+  baseURL: "https://hospedagem-api.onrender.com/api",
 });
 
 api.interceptors.request.use(
