73e2c817488bed6b867a1ebcfd857808f07585e8
Andrey Luiz Pereira <andreylpereira@gmail.com>
2025-06-10 15:42:02 -0300

    [ADD] Layout loading componente amenidades.

--- src/pages/amenity/modals/CreateAmenityModal.jsx
+++ src/pages/amenity/modals/CreateAmenityModal.jsx
@@ -91,13 +91,15 @@ const CreateAmenityModal = ({ isVisible, onClose, fetchAmenities }) => {
                     Fechar
                   </button>
                   {isLoading ? (
-                    <div className="pl-3">
+                    <div>
+                      <button disabled>
                       <div
                         className="spinner-border spinner-border-sm text-info"
                         role="status"
                       >
                         <span className="visually-hidden">Loading...</span>
                       </div>
+                      </button>
                     </div>
                   ) : (
                     <button
--- src/pages/amenity/modals/EditAmenityModal.jsx
+++ src/pages/amenity/modals/EditAmenityModal.jsx
@@ -103,13 +103,15 @@ const EditAmenityModal = ({
                     Fechar
                   </button>
                   {isLoading ? (
-                    <div className="pl-3">
+                    <div>
+                      <button disabled>
                       <div
                         className="spinner-border spinner-border-sm text-info"
                         role="status"
                       >
                         <span className="visually-hidden">Loading...</span>
                       </div>
+                      </button>
                     </div>
                   ) : (
                     <button
